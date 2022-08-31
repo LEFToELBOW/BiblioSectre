3b5e2f96d22581c6c241b18b8b724043fa2d867a
Patrick <patdelrio05@gmail.com>
2022-08-31 14:34:45 -0400

    Added random book instantiation

--- Assets/Scripts/LevelSet.cs
+++ Assets/Scripts/LevelSet.cs
@@ -4,22 +4,44 @@ using UnityEngine;
 
 public class LevelSet : MonoBehaviour
 {
-    public GameObject redBook, blueBook, greenBook;
+    [SerializeField] private GameObject redBook, blueBook, greenBook;
     private float level = 1f;
+    public GameObject[] books;
 
-    private void start()
+    private void Start()
     {
-        redBook = GameObject.Find("Red Book");
-        blueBook = GameObject.Find("Blue Book");
-        greenBook = GameObject.Find("Green Book");
-    }
 
-    void OnTriggerEnter2D(Collider2D col)
+        books = new GameObject[3];
+        books[0] = redBook;
+        books[1] = blueBook;
+        books[2] = greenBook;
+    }
+        
+    private void OnTriggerEnter2D(Collider2D col)
     {
         switch(level)
         {
             case 1:
-                Instantiate(redBook, new Vector2(0,0), Quaternion.identity);
+                Debug.Log(books.Length);
+
+                Instantiate(books[Random.Range(0,3)], new Vector2(Random.Range(-20, -17), Random.Range(10, 15)), Quaternion.identity);
+                Instantiate(books[Random.Range(0,3)], new Vector2(Random.Range(17, 20), Random.Range(10, 15)), Quaternion.identity);
+                Instantiate(books[Random.Range(0,3)], new Vector2(Random.Range(-12, -8), Random.Range(10, 15)), Quaternion.identity);
+                Instantiate(books[Random.Range(0,3)], new Vector2(Random.Range(8, 12), Random.Range(10, 15)), Quaternion.identity);
+
+                Instantiate(books[Random.Range(0,3)], new Vector2(Random.Range(-20, -17), Random.Range(15, 20)), Quaternion.identity);
+                Instantiate(books[Random.Range(0,3)], new Vector2(Random.Range(17, 20), Random.Range(15, 20)), Quaternion.identity);
+                Instantiate(books[Random.Range(0,3)], new Vector2(Random.Range(-12, -8), Random.Range(15, 20)), Quaternion.identity);
+                Instantiate(books[Random.Range(0,3)], new Vector2(Random.Range(8, 12), Random.Range(15, 20)), Quaternion.identity);
+                
+                Instantiate(books[Random.Range(0,3)], new Vector2(Random.Range(-20, -17), Random.Range(20, 25)), Quaternion.identity);
+                Instantiate(books[Random.Range(0,3)], new Vector2(Random.Range(17, 20), Random.Range(20, 25)), Quaternion.identity);
+                Instantiate(books[Random.Range(0,3)], new Vector2(Random.Range(-12, -8), Random.Range(20, 25)), Quaternion.identity);
+                Instantiate(books[Random.Range(0,3)], new Vector2(Random.Range(8, 12), Random.Range(20, 25)), Quaternion.identity);
+
+                
+                Debug.Log("Something is happenning");
+                level++;
                 break;
         }
     }
