583fe5f1065ffe71b74c17ae90294b367ba2457e
Patrick <patdelrio05@gmail.com>
2022-09-04 22:18:47 -0400

    pushing some stuff

--- Assets/Scripts/LevelSet.cs
+++ Assets/Scripts/LevelSet.cs
@@ -48,6 +48,25 @@ public class LevelSet : MonoBehaviour
                 level++;
                 break;
             case 2:
+                Debug.Log("Going to Level 2");
+
+                player.transform.position = new Vector2(0, 50);
+
+                Instantiate(books[Random.Range(0,3)], new Vector2(Random.Range(-20, -17), Random.Range(5, 10)), Quaternion.identity);
+                Instantiate(books[Random.Range(0,3)], new Vector2(Random.Range(17, 20), Random.Range(5, 10)), Quaternion.identity);
+                Instantiate(books[Random.Range(0,3)], new Vector2(Random.Range(-12, -8), Random.Range(5, 10)), Quaternion.identity);
+                Instantiate(books[Random.Range(0,3)], new Vector2(Random.Range(8, 12), Random.Range(5, 10)), Quaternion.identity);
+
+                Instantiate(books[Random.Range(0,3)], new Vector2(Random.Range(-20, -17), Random.Range(15, 20)), Quaternion.identity);
+                Instantiate(books[Random.Range(0,3)], new Vector2(Random.Range(17, 20), Random.Range(15, 20)), Quaternion.identity);
+                Instantiate(books[Random.Range(0,3)], new Vector2(Random.Range(-12, -8), Random.Range(15, 20)), Quaternion.identity);
+                Instantiate(books[Random.Range(0,3)], new Vector2(Random.Range(8, 12), Random.Range(15, 20)), Quaternion.identity);
+                
+                Instantiate(books[Random.Range(0,3)], new Vector2(Random.Range(-20, -17), Random.Range(25, 30)), Quaternion.identity);
+                Instantiate(books[Random.Range(0,3)], new Vector2(Random.Range(17, 20), Random.Range(25, 30)), Quaternion.identity);
+                Instantiate(books[Random.Range(0,3)], new Vector2(Random.Range(-12, -8), Random.Range(25, 30)), Quaternion.identity);
+                Instantiate(books[Random.Range(0,3)], new Vector2(Random.Range(8, 12), Random.Range(25, 30)), Quaternion.identity);
+                
                 break;
             case 3:
                 break;
